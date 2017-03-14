e9d3d92677de9c6e3bff8afa56f8f6ca71a26186
Laora <laora.gillesreynet@gmail.com>
2017-03-14 09:20:10 +0100

    Version 14/03 matin

--- src/main/java/com/polytech/config/ApplicationConfig.java
+++ src/main/java/com/polytech/config/ApplicationConfig.java
@@ -8,20 +8,21 @@ import com.polytech.business.PublicationService;
 import com.polytech.business.PublicationServiceImpl;
 import com.polytech.repository.PostRepository;
 import com.polytech.repository.JdbcPostRepository;
-import org.springframework.beans.factory.config.EmbeddedValueResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.activation.DataSource;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public DataSource dataSource(){
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build() ;
+    public EmbeddedDatabase dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("create-schema.sql")
+                .build() ;
     }
 
     @Bean
