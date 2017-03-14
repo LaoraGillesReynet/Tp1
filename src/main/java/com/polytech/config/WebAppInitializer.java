09d89d683ef0d5bec445f4ca979edafd5a133bce
Laora <laora.gillesreynet@gmail.com>
2017-03-14 16:59:37 +0100

    Version 2  14/03 pm

---
+++ src/main/java/com/polytech/config/WebAppInitializer.java
@@ -0,0 +1,31 @@
+package com.polytech.config;
+
+/**
+ * Created by Laora on 14/03/2017.
+ */
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{ApplicationConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        servletContext.setInitParameter("spring.profiles.active", "DEV");
+    }
+}
